dcca760573d16bc90f3757ff8b7e380917928ebf
b-nana <babenkoaa97@gmail.com>
2022-07-14 14:27:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+        public static void main(String[] args) {
+
+            int ticketPrice = 15_000;
+            int rublesForOneMile = 20;
+
+            int miles = ticketPrice / rublesForOneMile;
+
+
+            System.out.println(miles);
+        }
+    }
+
