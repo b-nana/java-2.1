adb8111e99b683d365b5d877dbdd7c224f246ac6
b-nana <babenkoaa97@gmail.com>
2022-07-18 13:53:11 +0300

    BonusMilesService added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
-        public static void main(String[] args) {
-
-            int ticketPrice = 15_000;
-            int rublesForOneMile = 20;
-
-            int miles = ticketPrice / rublesForOneMile;
-
-
-            System.out.println(miles);
-        }
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
+}
 
